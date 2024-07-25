1bdeee194983956995e2c8a60e9c4ad4d3b33824
Lev <gaz358@Home-PC.>
2024-07-25 08:30:25 +0300

    n

--- install-connector.sh
+++ install-connector.sh
@@ -9,7 +9,10 @@ done
 # Install the connector
 echo "Installing the connector..."
 curl -X POST -H "Content-Type: application/json" --data @/usr/share/confluent/docker/postgres-connector2.json http://kafka-connect:8083/connectors
+
+# Wait a bit for the connector to be installed
 sleep 3
+
 # Check installation status
 echo "Connector installation status:"
 curl -X GET http://kafka-connect:8083/connectors/postgres-connector2/status

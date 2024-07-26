2a1c1a9b7a79ae4dd5a11a43d825869273dfbeeb
Lev <gaz358@Home-PC.>
2024-07-26 08:45:23 +0300

    Коннектор на автомате

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Запустить контейнеры в фоновом режиме
+echo "Запуск контейнеров..."
+docker-compose up -d
+
+chmod +x install-connector.sh
+
+
+# Проверка, доступен ли порт 8083
+echo "Ожидание запуска Kafka Connect на порту 8083..."
+until curl -s http://localhost:8083/ | grep -q '"version"'; do
+  sleep 5
+done
+
+# Выполнить скрипт установки коннектора
+echo "Запуск скрипта install-connector.sh..."
+docker exec -it kafka-connect /usr/share/confluent/docker/install-connector.sh
+
+echo "Скрипт завершен."

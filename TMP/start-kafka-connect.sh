f10ba3d767551b0fe9ca54249c1dea52982553e8
Lev <gaz358@Home-PC.>
2024-07-29 10:44:39 +0300

    n

--- TMP/start-kafka-connect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Подождем, пока Kafka Connect не станет доступен
-echo "Waiting for Kafka Connect to start..."
-sleep 20
-
-# Запускаем Kafka Connect
-echo "Starting Kafka Connect..."
-/usr/local/bin/connect-distributed /etc/kafka/connect-distributed.properties

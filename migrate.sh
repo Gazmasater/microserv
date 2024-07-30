4c8fd5581eb52d798e15ed0d65d350dea769e0b6
Lev <129895474+Gazmasater@users.noreply.github.com>
2024-07-30 08:33:53 +0300

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,12 +2,6 @@
 
 apt-get update
 apt-get install -y iproute2
-
-#echo 'Waiting for Postgres to be ready...'
-#while ! ss -tuln | grep -q ':5432'; do
-#    sleep 4
-#done
-
 echo 'Postgres is ready, running migrations'
 go install github.com/pressly/goose/v3/cmd/goose@latest
 export PATH=$PATH:$GOPATH/bin
